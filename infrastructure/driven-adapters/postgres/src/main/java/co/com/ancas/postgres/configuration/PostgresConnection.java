e10d22cc51ae121acc6bcfb4eb29281a8d2bc010
ancas19 <andres.cmonsalve@gmail.com>
2024-10-04 23:18:58 -0500

    Feat: Add files to create docker image

---
+++ infrastructure/driven-adapters/postgres/src/main/java/co/com/ancas/postgres/configuration/PostgresConnection.java
@@ -0,0 +1,71 @@
+package co.com.ancas.postgres.configuration;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "co.com.ancas.postgres.repositories",
+        entityManagerFactoryRef = "libraryEntityManager",
+        transactionManagerRef = "libraryTransactionManager"
+)
+public class PostgresConnection {
+    @Value("${spring.jpa.show-sql}")
+    private String showSql;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String hbm2ddlAuto;
+    @Value("${postgres.dialect}")
+    private String dialect;
+    @Value("${postgres.driverClassName}")
+    private String className;
+    @Value("${postgres.url}")
+    private String jdbcUrl;
+    @Value("${postgres.user}")
+    private String jdbcUser;
+    @Value("${postgres.pass}")
+    private String jdbcPass;
+    @Bean
+    public DataSource dataSource()  {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(className);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(jdbcUser);
+        dataSource.setPassword(jdbcPass);
+        return dataSource;
+    }
+
+
+    @Bean(name = "secutiryEntityManager")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("co.com.ancas.postgres.entities");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", dialect);
+        properties.setProperty("hibernate.show_sql",showSql);
+        properties.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
+        em.setJpaProperties(properties);
+        return em;
+    }
+
+    @Bean(name = "libraryTransactionManager")
+    @Primary
+    public PlatformTransactionManager transactionManager(@Qualifier("libraryEntityManager") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
