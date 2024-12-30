2921da6f2e5636ec8a0709efcf55faab58077b4c
ancas19 <andres.cmonsalve@gmail.com>
2024-12-29 21:59:41 -0500

    Feat: Create java clases to manipulate book's information

---
+++ domain/models/src/main/java/co/com/ancas/models/model/Book.java
@@ -0,0 +1,23 @@
+package co.com.ancas.models.model;
+
+import jakarta.persistence.Column;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Book {
+    private Long id;
+    private String title;
+    private String isbn;
+    private Long authorId;
+    private LocalDate publishDate;
+    private Long genreId;
+    private Integer availableCopies;
+    private String blurb;
+    private Long imageId;
+}
