0d335fc4affe05aa8d85e79d1dcc1baae69b599d
ancas19 <andres.cmonsalve@gmail.com>
2025-01-24 22:57:22 -0500

    Feat: Enable asybnc programming

--- infrastructure/entrypoints/src/main/java/co/com/ancas/Main.java
+++ infrastructure/entrypoints/src/main/java/co/com/ancas/Main.java
@@ -3,7 +3,9 @@ package co.com.ancas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @ComponentScan("co.com.ancas.*")
 public class Main {
